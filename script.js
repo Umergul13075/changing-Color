119f30c0e27dd8b272c2c07b4044b3ef2f9c587a
Umergul13075 <145903053+Umergul13075@users.noreply.github.com>
2025-05-17 00:01:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// generate a random color
+const randomColor = function(){
+  const hex = "0123456789ABCDEF";
+  // let color = "#ffffff";
+  let color = "#";
+  for(let i =0 ; i<6; i++){
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+let intervalid;
+const startChangingColor = function(){
+  intervalid = setInterval(changeBgColor, 1000);
+
+  function changeBgColor(){
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+// stop changing color
+const stopChangingColor = function(){
+  clearInterval(intervalid);
+  intervalid = null;
+};
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
